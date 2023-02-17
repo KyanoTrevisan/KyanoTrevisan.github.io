a646fafc84f71b81976a7f67f3b94b07a6c2fb49
KyanoTrevisan <r0894962@student.thomasmore.be>
2023-02-17 21:47:01 +0100

    fix home page animation

--- scripts/typing-animation.js
+++ scripts/typing-animation.js
@@ -2,12 +2,22 @@ var typingAnimation = document.querySelector('.typing-animation');
 var text = typingAnimation.textContent;
 typingAnimation.textContent = '';
 
-var i = 0;
-var timer = setInterval(function() {
-  if (i < text.length) {
-    typingAnimation.textContent += text.charAt(i);
-    i++;
-  } else {
-    clearInterval(timer);
-  }
-}, 100);
+var timer;
+
+function startTypingAnimation() {
+  var i = 0;
+  timer = setInterval(function() {
+    if (i < text.length) {
+      typingAnimation.textContent += text.charAt(i);
+      i++;
+    } else {
+      clearInterval(timer);
+      setTimeout(function() {
+        typingAnimation.textContent = '';
+        startTypingAnimation();
+      }, 5000); // Wait for 20 seconds before starting the animation again
+    }
+  }, 50);
+}
+
+startTypingAnimation();
